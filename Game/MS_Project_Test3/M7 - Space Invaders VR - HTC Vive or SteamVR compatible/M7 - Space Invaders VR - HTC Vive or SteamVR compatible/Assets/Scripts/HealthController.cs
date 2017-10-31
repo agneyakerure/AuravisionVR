887bd2ad652345c7970159bddd34931c963f7e27
Agneya Kerure <kerure.agneya@gmail.com>
2017-10-31 03:18:18 -0400

    game 3 added with longnames enabled

---
+++ Game/MS_Project_Test3/M7 - Space Invaders VR - HTC Vive or SteamVR compatible/M7 - Space Invaders VR - HTC Vive or SteamVR compatible/Assets/Scripts/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+    [SerializeField] private float health = 20f;
+
+	// Use this for initialization
+	public void ApplyDamage(float dmg)
+    {
+        health = health - dmg;
+        //if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
